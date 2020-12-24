pombase/chobo

commit 543dd4b69c775fcd8b630f482080446684fc80c7

Remove backslash escapes in dbxref accessions
Refs pombase/website#589

lib/PomBase/Chobo/Role/ChadoStore.pm
@@ -153,6 +153,7 @@ my %row_makers = (
         if (!defined $accession) {
           die "accession is null for accession in db: $db_name\n";
         }
+        $accession =~ s|\\(.)|$1|g;
         [$db_id, $accession];
       } @ont_db_termids;
     } $ontology_data->get_db_names();

commit fbbd912c6d0c07ebe42e76e3016bfa274b9a8f72

Fix bug caused by 543dd4b69c775fcd8b
We were de-quoting at the wrong point so ended up with quoted and
un-quoted def xrefs in different places.

lib/PomBase/Chobo/OntologyConf.pm
@@ -59,6 +59,11 @@ our %field_conf = (
         my $dbxrefs = $2 // '';
 
         my @dbxrefs =
+          map {
+            # remove quoting
+            s|\\(.)|$1|g;
+            $_;
+          }
           grep {
             !m|^(?:url:)?https?\\?:| && /^\S+:\S+$/;
           } split /\s*,\s/, $dbxrefs;

lib/PomBase/Chobo/Role/ChadoStore.pm
@@ -153,7 +153,6 @@ my %row_makers = (
         if (!defined $accession) {
           die "accession is null for accession in db: $db_name\n";
         }
-        $accession =~ s|\\(.)|$1|g;
         [$db_id, $accession];
       } @ont_db_termids;
     } $ontology_data->get_db_names();
@@ -252,7 +251,7 @@ my %row_makers = (
         my $dbxref_details = $chado_data->get_dbxref_by_termid($id);
 
         if (!defined $dbxref_details) {
-          die "no dbxref details for $id";
+          die "no dbxref details for $id ", $term->name(), "\n";
         }
 
         my $dbxref_id = $dbxref_details->{dbxref_id};